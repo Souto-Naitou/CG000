cba009913f830dbbecae669221065de0fdfb9cdc
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-02-13 23:15:48 +0900

    2.00.04

--- main.cpp
+++ main.cpp
@@ -1,10 +1,86 @@
 #include <Windows.h>
+#include <cstdint>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 // Windowsアプリでのエントリポイント
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	WNDCLASS wc{};
+	// ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	// ウィンドウクラス名(なんでもおけ)
+	wc.lpszClassName = L"CG2WindowClass";
+	// インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	// カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	// ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+
+	// クライアント領域のサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	// ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
+
+	// クライアント領域をもとに実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,		// 利用するクラス名
+		L"CG2",					// タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,	// よく見るウィンドウスタイル
+		CW_USEDEFAULT,			// 表示X座標(ウィンドウ出現位置？)
+		CW_USEDEFAULT,			// 表示Y座標
+		wrc.right - wrc.left,	// ウィンドウ横幅
+		wrc.bottom - wrc.top,	// ウィンドウ縦幅
+		nullptr,				// 親ウィンドウハンドル(子ウィンドウ作るときに使うかも？)
+		nullptr,				// メニューハンドル
+		wc.hInstance,			// インスタンスハンドル	
+		nullptr					// オプション
+	);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};		// MSG構造体 OSから受け取る？
+
+	while (msg.message != WM_QUIT)
+	{
+		// Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);	// メッセージを解釈？
+			DispatchMessage(&msg);			// デキュー的な？
+		}
+		else
+		{
+			// ゲームの処理
+		}
+	}
+
 	// 出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello, DirectX!\n");
 
 	return 0;
+}
+
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	
+	// メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+	case WM_DESTROY:	// ウィンドウが破壊された
+		// OSに対して、アプリ終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	// 標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+	
 }
